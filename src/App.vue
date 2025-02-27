210037535cb3e84d3ab47b80c8edd80c4d277e7d
brandonoakley44 <brandonoakley324@gmail.com>
2025-02-27 09:27:00 -0400

    adding capacitor camera plug in

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { Camera, CameraResultType } from '@capacitor/camera';
 </script>
 
 <template>
-  <div class="text-red-400">hello that lol much better mega merge</div>
+  <div class="text-red-400">hello that lol much better fef merge</div>
 </template>
 
 <style scoped>
